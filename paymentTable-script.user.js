8a9cf5b57dfc41f51388576845e483fadb20c284
Jose Bujalance <joseanbu23@gmail.com>
2018-04-14 12:53:38 +0200

    Script metadata

---
+++ paymentTable-script.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name Biwenger Payment Table Script
+// @namespace com.bujalance.scripts
+// @description Provides information of the payment of each user in the League clasificatiton table
+// @match https://biwenger.as.com/league
+// @require https://code.jquery.com/jquery-3.3.1.js
+// @version 0.1
+// ==/UserScript==
+
+function onDocumentReady () {
+    console.log("da shit is ready");
+}
+
+$(document).ready(onDocumentReady);
